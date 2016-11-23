4043467396933f859954df761fcc604da7a04c0c
rmay <majway27.1@gmail.com>
2016-11-23 16:56:46 +0000

    POC Milestone - can now schedule an appointment with hardcoded values.  Numerous changes include Finalize call, additional config.yaml lines

--- main.rb
+++ main.rb
@@ -31,6 +31,8 @@ my_page = a.submit(report_form)
 
 ## Set other key data mimicing "javascript:doSubmit() that validates and posts".  Mechanize doesn't support Javascript.  Set Other hidden data on form
 ## 11-23 RM, use Mechanize#post, for some reason submitting the report_form object(like above) doesn't work. Notably uri picks up a trailing action value and the action field on the form isn't set.
+## This selects an available day to set a reservation.
+## Returns day's available slots for reservation.
 my_page = a.post config['host'] + config['instance'] + '/', 
   "selection_form" => "yes", 
   "d" => "appointplus356",
@@ -61,6 +63,72 @@ my_page = a.post config['host'] + config['instance'] + '/',
   "previous_service_id" => config['service_id'],
   # Set desired appointment date
   "date_ymd" => config["date_ymd"]
+
+## Choose/submit time slot on day's calendar
+## Checks a time slots availability, and if availabile soft-reserves it.
+## Returns confirmation to be finalized below.
+my_page = a.post config['host'] + config['instance'] + '/', 
+  "page" => "10",
+  "customer_id" => config["customer_id"],
+  "service_id" => config['service_id'],
+  "e_id" => config["e_id"],
+  "appt_e_id" => config["e_id"],
+  "starting_date" => config["starting_date"],
+  "selected_children" => config["selected_children"],
+  "child_count" => config["child_count"],
+  "children_list" => config["children_list"],
+  "customer_location_id" => config["location_id"],
+  "location_id" => config["location_id"],
+  "id" => config["location_id"],
+  "c_id" => config["location_id"],
+  "selected_location_id" => config["location_id"],
+  "headquarters_id" => config["location_id"],
+  "first_appt_time" => config["first_appt_time"],
+  "last_appt_time" => config["last_appt_time"],
+  "spots" => "1",
+  "method" => "2",
+  "auth" => "yes",
+  # "action", aka request type
+  "action" => "confirm",
+  "appt_date" => config["date_ymd"],
+  "appt_start_time" => config["appt_start_time"],
+  "appt_end_time" => config["appt_end_time"],
+  "dropdown_e_id" => config["e_id"]
+
+## Finalizes a selected reservation.
+## Returns success message if reservation was successful.
+my_page = a.post config['host'] + config['instance'] + '/', 
+  "id" => config["location_id"],
+  "d" => "appointplus356",
+  "m" => "2",
+  "type" => "23",
+  "page" => "10",
+  "customer_id" => config["customer_id"],
+  "auth" => "yes",
+  "service_id" => config['service_id'],
+  "starting_date2" => config["starting_date"],
+  "e_id" => config["e_id"],
+  "appt_e_id" => config["e_id"],
+  "location_id" => config["location_id"],
+  "customer_location_id" => config["location_id"],
+  "headquarters_id" => config["location_id"],
+  "selected_children" => config["selected_children"],
+  "child_count" => config["child_count"],
+  "children_list" => config["children_list"],
+  "first_appt_time" => config["first_appt_time"],
+  "last_appt_time" => config["last_appt_time"],
+  "appt_date" => config["date_ymd"],
+  "appt_start_time" => config["appt_start_time"],
+  "appt_end_time" => config["appt_end_time"],
+  # This instances implementation does separate appointments for each asset.  Spots will be 1.
+  "spots" => "1",
+  "validate_cc" => "no",
+  "starting_date1" => config["starting_date"],
+  # "action", aka request type
+  "action" => "confirm",
+  "notes" => " ",
+  # Finalize Action
+  "finalize_appt" => "Finalize Appointment"
 
 # Debug
 pp my_page
