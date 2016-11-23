c16ed76d12ae5791f43a319c027017ae8d7286d6
rmay <majway27.1@gmail.com>
2016-11-23 13:29:19 +0000

    Two changes - 1) Moved host into config file, 2) Built out post to retrieve day availability, tested/works.

--- main.rb
+++ main.rb
@@ -12,7 +12,7 @@ a = Mechanize.new { |agent|
 }
 
 # Instance, User, Pass
-my_page = a.get('https://booknow.appointment-plus.com/' +  config['instance'] + '/').form_with(:name => 'auth_form') do |form|
+my_page = a.get(config['host'] + config['instance'] + '/').form_with(:name => 'auth_form') do |form|
   form['loginname'] = config["user"]
   form['password'] = config["pass"]
 end.submit
@@ -29,41 +29,41 @@ report_form = my_page.form('myForm1')
 report_form.e_id = config["e_id"]
 my_page = a.submit(report_form)
 
-# Set Children
-report_form = my_page.form('myForm1')
-# Check as many checkboxes as needed
-report_form.checkbox_with(config["child1"]).check
-report_form.checkbox_with(config["child2"]).check
-
-## Set other key data mimicing "javascript:doSubmit() that validates and posts".  Mechanize doesn't support Javascript.
-# Set desired appointment date
-report_form.date_ymd = config["date_ymd"]
-report_form.starting_date = config["starting_date"]
-# "action", aka request type
-report_form.action = 'viewappts'
-
-## Other hidden data on form
-report_form.selection_form = 'yes'
-report_form.d = 'appointplus356'
-report_form.page = '10'
-report_form.m = '2'
-report_form.type = '23'
-report_form.auth = 'yes'
-report_form.customer_id = config["customer_id"]
-report_form.day_name = 'any'
-report_form.location_id = config["location_id"]
-report_form.id = config["location_id"]
-report_form.next_date = config["next_date"]
-report_form.prev_date = config["prev_date"]
-report_form.next_date = config["next_date1"]
-report_form.prev_date = config["prev_date1"]
-report_form.next_date = config["next_date1"]
-report_form.prev_date = config["prev_date1"]
-
-my_page = a.submit(report_form)
+## Set other key data mimicing "javascript:doSubmit() that validates and posts".  Mechanize doesn't support Javascript.  Set Other hidden data on form
+## 11-23 RM, use Mechanize#post, for some reason submitting the report_form object(like above) doesn't work. Notably uri picks up a trailing action value and the action field on the form isn't set.
+my_page = a.post config['host'] + config['instance'] + '/', 
+  "selection_form" => "yes", 
+  "d" => "appointplus356",
+  "page" => "10",
+  "m" => "2",
+  "type" => "23",
+  "auth" => "yes",
+  # "action", aka request type
+  "action" => "viewappts",
+  "customer_id" => config["customer_id"],
+  "customer_location_id" => config["location_id"],
+  "day_name" => "any",
+  "location_id" => config["location_id"],
+  "id" => config["location_id"],
+  "headquarters_id" => config["location_id"],
+  "service_id" => "974",
+  "e_id" => "782",
+  # Set applicable children here. >0 children required.
+  config["child1"] => "on",
+  config["child2"] => "on",
+  "next_date" => config["next_date"],
+  "prev_date" => config["prev_date"],
+  #"next_date" => config["next_date1"],
+  #"prev_date" => config["prev_date1"],
+  #"next_date" => config["next_date1"],
+  #"prev_date" => config["prev_date1"],
+  "starting_date" => config["starting_date"],
+  "previous_service_id" => "974",
+  # Set desired appointment date
+  "date_ymd" => config["date_ymd"]
 
 # Debug
-#pp my_page
+pp my_page
 
 #my_page.links.each do |link|
 #  puts link.text
